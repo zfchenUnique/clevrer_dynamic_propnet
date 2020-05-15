2fc16dd9e70fad13e2d4d3e212b1cf26101e6091
JeffChen2017 <chenzhenfang2013@gmail.com>
2020-05-15 00:07:52 -0400

    latent for proposals

--- scripts/train_tube_latent.sh
+++ scripts/train_tube_latent.sh
@@ -6,14 +6,12 @@ jac-crun ${GPU_ID} python train_tube_latent.py \
 --data_dir /home/zfchen/code/nsclClevrer/clevrer/ \
 --prp_dir /home/zfchen/code/nsclClevrer/clevrer/proposals \
 --ann_dir /home/zfchen/code/nsclClevrer/clevrer \
---tube_dir /home/zfchen/code/nsclClevrer/clevrer/tubeProposalsGt \
 --tube_mode 1 \
 --batch_size 1 \
---outf dumps/latent_norm_ftr_001 \
+--outf dumps/prpRefine_latent_norm_ftr_n_his_3\
 --desc /home/zfchen/code/nsclClevrer/dynamicNSCL/clevrer/desc_nscl_derender_clevrer_v2.py \
 --rel_box_flag 0 --dynamic_ftr_flag 1 --version v3 \
 --dataset clevrer \
---load /home/zfchen/code/nsclClevrer/dynamicNSCL/dumps/remote_models/frm_31_epoch_24.pth \
 --num_workers 2 \
 --data_ver v3 \
 --resume_epoch 0 \
@@ -21,8 +19,14 @@ jac-crun ${GPU_ID} python train_tube_latent.py \
 --ckp_per_iter 100000 \
 --colli_ftr_only 1 \
 --norm_ftr_flag 1 \
---lr 0.001
+--lr 0.0001 \
+--load /home/zfchen/code/nsclClevrer/dynamicNSCL/dumps/remote_models/refine_epoch_10.pth \
+--tube_dir /home/zfchen/code/nsclClevrer/clevrer/tubeProposalsAttrV0/1.0_1.0_0.5 \
+--frame_offset 4 \
+--n_his 3
 #--debug 1
+#--tube_dir /home/zfchen/code/nsclClevrer/clevrer/tubeProposalsGt \
 #--resume_model_full_path latent_prp_CLEVRER_noAttr_noEdgeSuperv_pn_pstep_2_tubemode_1/tube_net_epoch_0_iter_500000.pth \
+#--load /home/zfchen/code/nsclClevrer/dynamicNSCL/dumps/remote_models/frm_31_epoch_24.pth \
 #--debug 1
 #CUDA_VISIBLE_DEVICES=${GPU_ID} python train_tube_latent.py \
