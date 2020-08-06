f4dbc9bf89ae6f7139a77c871a8d354e2a76d1a6
zfchen <zfchen@cs.hku.hk>
2020-08-06 16:07:39 +0800

    eval tube script

--- scripts/eval_no_edge_superv_tube.sh
+++ scripts/eval_no_edge_superv_tube.sh
@@ -1,4 +1,6 @@
 GPU_ID=$1
+#outf_path=tubeNetAttrV3_offset4_lam_position_1000
+outf_path=tubeNetAttrV3_offset4_lam_position_100
 CUDA_VISIBLE_DEVICES=${GPU_ID} python eval_tube.py \
     --store_img 1 \
     --tube_mode 1 \
@@ -9,22 +11,23 @@ CUDA_VISIBLE_DEVICES=${GPU_ID} python eval_tube.py \
     --prp_dir /home/zfchen/code/nsclClevrer/clevrer/proposals \
     --ann_dir /home/zfchen/code/nsclClevrer/clevrer \
     --tube_dir /home/zfchen/code/nsclClevrer/clevrer/tubeProposalsAttrV3/1.0_1.0_0.4_0.7 \
-    --box_only_flag 0 \
-    --epoch 0 \
-    --iter 0 \
-    --state_dim 7 \
-    --relation_dim 5 \
-    --video 0 \
-    --des_dir dumps/attrV3_offset4\
-    --evalf dumps/visualization_AttrV3_Offset4 \
-    --outf dumps/tubeNetAttrV3True_offset4 \
-    --epoch 0 \
-    --iter 0 \
-    --frame_offset 4 \
+    --video 1 \
     --store_patch_flag 1 \
     --st_idx 10000 \
     --ed_idx 15000 \
-    #--outf dumps/tubeNetAttrV3True_offset4 \
+    --box_only_flag 0 \
+    --state_dim 7 \
+    --relation_dim 5 \
+    --new_mode 0 \
+    --frame_offset 5 \
+    --outf dumps/${outf_path} \
+    --des_dir dumps/annos/${outf_path} \
+    --evalf dumps/visualization/${outf_path} \
+    --debug 1 \
+    --epoch 0 \
+    --iter 0 \
+    #--outf dumps/tube_new_mode1_offset5 \
+    #--outf dumps/tubeNetGt_offset4 \
     #--outf dumps/box_only_tubeGt \
     #--outf dumps/gt \
     #--tube_dir /home/zfchen/code/nsclClevrer/clevrer/tubeProposalsGt \
