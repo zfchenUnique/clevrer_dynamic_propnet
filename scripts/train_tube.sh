649d80db18084b52330ec34446a66cf2b9dc766d
Zhenfang Chen <u3004417@gpu2gate1.gpufarm2.cs.hku.hk>
2020-08-18 17:14:20 +0800

    scripts/train_tube

--- scripts/train_tube.sh
+++ scripts/train_tube.sh
@@ -9,12 +9,13 @@ CUDA_VISIBLE_DEVICES=${GPU_ID} python train_tube.py \
 --num_workers 1 \
 --tube_mode 1 \
 --batch_size 1 \
---box_only_flag 0 \
+--box_only_flag 1 \
 --frame_offset 4 \
 --relation_dim 5 \
 --state_dim 4 \
---outf dumps/tubeNetAttrV3_offset4_lam_position_1000 \
---tube_dir /home/zfchen/code/nsclClevrer/clevrer/tubeProposalsAttrV3/1.0_1.0_0.4_0.7 \
+--outf dumps/offset5_matchNoIoU \
+--tube_dir ../../clevrer/tubeProposalsAttrMatchNoIoUThre/1.0_1.0_0.6_0.7 \
+#--tube_dir /home/zfchen/code/nsclClevrer/clevrer/tubeProposalsAttrV3/1.0_1.0_0.4_0.7 \
 #--outf dumps/tubeNetAttrV3_offset5 \
 #--tube_dir /home/zfchen/code/nsclClevrer/clevrer/tubeProposalsGt \
 #--debug 1 \
